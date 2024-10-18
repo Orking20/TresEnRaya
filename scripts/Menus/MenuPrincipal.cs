2e1208951a4feb736c9091189eb57920b9f9a8ef
Orking20 <mateoluchetti@hotmail.com>
2024-10-19 00:44:07 +0200

    Se agrega funcionalidad al botón jugar

--- scripts/Menus/MenuPrincipal.cs
+++ scripts/Menus/MenuPrincipal.cs
@@ -3,20 +3,28 @@ using System;
 
 public partial class MenuPrincipal : Control
 {
+	private Button btnJugar;
 	private Button btnSalir;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.btnJugar = GetNode<Button>("BotonesCentrados/ContenedorBotones/BtnJugar");
 		this.btnSalir = GetNode<Button>("BotonesCentrados/ContenedorBotones/BtnSalir");
 		
 		//btnSalir.Connect("pressed", new Callable(this, nameof(OnBotonSalirPressed)));
+		this.btnJugar.Pressed += OnBotonJugarPressed;
 		this.btnSalir.Pressed += OnBotonSalirPressed;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void OnBotonJugarPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Escenas/Tablero.tscn");
 	}
 
 	private void OnBotonSalirPressed()
