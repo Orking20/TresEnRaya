e9bfd9e435377dc6822e7ef8fd3b67602e8bc82f
Orking20 <mateoluchetti@hotmail.com>
2024-10-19 19:11:46 +0200

    Se agrega inteligencia artificial con el algoritmo minimax

--- scripts/Tablero/Tablero.cs
+++ scripts/Tablero/Tablero.cs
@@ -39,10 +39,6 @@ public partial class Tablero : Control
 		}
 	}
 
-	public override void _Process(double delta)
-	{
-	}
-
 	private void OnCasillaPresionada(Button casilla)
 	{
 		if (this.turnoJugador)
@@ -50,10 +46,11 @@ public partial class Tablero : Control
 			casilla.Icon = this.fichaX;
 			casilla.Disabled = true;
 			this.turnoJugador = false;
-			ChequearVictoria();
+			ChequearVictoria(this.fichaX);
 			if (!this.finPartida)
 			{
 				TurnoMaquina();
+				ChequearVictoria(this.fichaO);
 			}
 		}
 		else
@@ -61,109 +58,188 @@ public partial class Tablero : Control
 			casilla.Icon = this.fichaO;
 			casilla.Disabled = true;
 			this.turnoJugador = true;
-			ChequearVictoria();
+			
 		}
 	}
 
 	private void TurnoMaquina()
 	{
-		bool fichaMarcada = false;
-		bool hayEspacio = false;
-		var fila = new Random().Next(0, 3);
-		var columna = new Random().Next(0, 3);
+		if (!HayEspaciosDisponibles())
+			return;
+		
+		int mejorPuntuacion = int.MinValue;
+		int mejorFila = -1;
+		int mejorColumna = -1;
 
+		// Probar todas las posibles jugadas
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (!casillas[i, j].Disabled)
+				{
+					// Simular la jugada
+					casillas[i, j].Icon = this.fichaO;
+					casillas[i, j].Disabled = true;
+
+					// Obtener puntuación de esta jugada
+					int puntuacion = Minimax(false);
+
+					// Deshacer la jugada
+					casillas[i, j].Icon = null;
+					casillas[i, j].Disabled = false;
+
+					// Actualizar la mejor jugada si encontramos una mejor
+					if (puntuacion > mejorPuntuacion)
+					{
+						mejorPuntuacion = puntuacion;
+						mejorFila = i;
+						mejorColumna = j;
+					}
+				}
+			}
+		}
+
+		if (mejorFila != -1 && mejorColumna != -1)
+		{
+			casillas[mejorFila, mejorColumna].EmitSignal(Button.SignalName.Pressed);
+			casillas[mejorFila, mejorColumna]._Pressed();
+		}
+	}
+
+	private int Minimax(bool esMaximizador)
+	{
+		// Verificar si hay un ganador o empate
+		if (HayGanadorMinimax(fichaO)) return 1;    // La máquina gana
+		if (HayGanadorMinimax(fichaX)) return -1;   // El jugador gana
+		if (!HayEspaciosDisponibles()) return 0;    // Empate
+
+		if (esMaximizador)
+		{
+			int mejorPuntuacion = int.MinValue;
+			
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (!casillas[i, j].Disabled)
+					{
+						// Simular jugada
+						casillas[i, j].Icon = fichaO;
+						casillas[i, j].Disabled = true;
+
+						mejorPuntuacion = Math.Max(mejorPuntuacion, Minimax(false));
+
+						// Deshacer jugada
+						casillas[i, j].Icon = null;
+						casillas[i, j].Disabled = false;
+					}
+				}
+			}
+			return mejorPuntuacion;
+		}
+		else
+		{
+			int mejorPuntuacion = int.MaxValue;
+			
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					if (!casillas[i, j].Disabled)
+					{
+						// Simular jugada
+						casillas[i, j].Icon = fichaX;
+						casillas[i, j].Disabled = true;
+
+						mejorPuntuacion = Math.Min(mejorPuntuacion, Minimax(true));
+
+						// Deshacer jugada
+						casillas[i, j].Icon = null;
+						casillas[i, j].Disabled = false;
+					}
+				}
+			}
+			return mejorPuntuacion;
+		}
+	}
+
+	private bool HayGanadorMinimax(Texture2D ficha)
+	{
+		// Revisar filas y columnas
+		for (int i = 0; i < 3; i++)
+		{
+			if ((casillas[i, 0].Icon == ficha && casillas[i, 1].Icon == ficha && casillas[i, 2].Icon == ficha) ||
+				(casillas[0, i].Icon == ficha && casillas[1, i].Icon == ficha && casillas[2, i].Icon == ficha))
+			{
+				return true;
+			}
+		}
+
+		// Revisar diagonales
+		if ((casillas[0, 0].Icon == ficha && casillas[1, 1].Icon == ficha && casillas[2, 2].Icon == ficha) ||
+			(casillas[0, 2].Icon == ficha && casillas[1, 1].Icon == ficha && casillas[2, 0].Icon == ficha))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	private void ChequearVictoria(Texture2D ficha)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (this.casillas[i, 0].Icon == ficha && this.casillas[i, 1].Icon == ficha && this.casillas[i, 2].Icon == ficha ||
+			this.casillas[0, i].Icon == ficha && this.casillas[1, i].Icon == ficha && this.casillas[2, i].Icon == ficha)
+			{
+				AnunciarVictoria(ficha);
+				this.finPartida = true;
+				return;
+			}
+		}
+
+		if (this.casillas[0, 0].Icon == ficha && this.casillas[1, 1].Icon == ficha && this.casillas[2, 2].Icon == ficha ||
+		this.casillas[0, 2].Icon == ficha && this.casillas[1, 1].Icon == ficha && this.casillas[2, 0].Icon == ficha)
+		{
+			AnunciarVictoria(ficha);
+			this.finPartida = true;
+			return;
+		}
+	}
+
+	private void AnunciarVictoria(Texture2D ficha)
+	{
+		foreach (var casilla in this.casillas)
+		{
+			casilla.Disabled = true;
+		}
+
+		Label ganador = GetNode<Label>("Ganador");
+
+		if (ficha == this.fichaX)
+		{
+			ganador.Text = "Victoria!";
+			ganador.Modulate = new Color(0, 255, 0);
+		}
+		else
+		{
+			ganador.Text = "Derrota";
+			ganador.Modulate = new Color(255, 0, 0);
+		}
+
+		ganador.AddThemeFontSizeOverride("font_size", 32);
+		ganador.HorizontalAlignment = HorizontalAlignment.Center;
+		ganador.VerticalAlignment = VerticalAlignment.Bottom;
+	}
+
+	private bool HayEspaciosDisponibles()
+	{
 		foreach (var casilla in this.casillas)
 		{
 			if (!casilla.Disabled)
-			{
-				hayEspacio = true;
-			}
+				return true;
 		}
-
-		while (!fichaMarcada && hayEspacio)
-		{
-			if (this.casillas[fila, columna].Disabled == false)
-			{
-				this.casillas[fila, columna].EmitSignal(Button.SignalName.Pressed);
-				this.casillas[fila, columna]._Pressed();
-				fichaMarcada = true;
-			}
-			else
-			{
-				fila = new Random().Next(0, 3);
-				columna = new Random().Next(0, 3);
-			}
-		}
-	}
-
-	private void ChequearVictoria()
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			if (this.casillas[i, 0].Icon == this.fichaX && this.casillas[i, 1].Icon == this.fichaX && this.casillas[i, 2].Icon == this.fichaX ||
-			this.casillas[0, i].Icon == this.fichaX && this.casillas[1, i].Icon == this.fichaX && this.casillas[2, i].Icon == this.fichaX)
-			{
-				AnunciarVictoria();
-				this.finPartida = true;
-				return;
-			}
-		}
-
-		if (this.casillas[0, 0].Icon == this.fichaX && this.casillas[1, 1].Icon == this.fichaX && this.casillas[2, 2].Icon == this.fichaX ||
-		this.casillas[0, 2].Icon == this.fichaX && this.casillas[1, 1].Icon == this.fichaX && this.casillas[2, 0].Icon == this.fichaX)
-		{
-			AnunciarVictoria();
-			this.finPartida = true;
-			return;
-		}
-
-		for (int i = 0; i < 3; i++)
-		{
-			if (this.casillas[i, 0].Icon == this.fichaO && this.casillas[i, 1].Icon == this.fichaO && this.casillas[i, 2].Icon == this.fichaO ||
-			this.casillas[0, i].Icon == this.fichaO && this.casillas[1, i].Icon == this.fichaO && this.casillas[2, i].Icon == this.fichaO)
-			{
-				AnunciarDerrota();
-				this.finPartida = true;
-				return;
-			}
-		}
-
-		if (this.casillas[0, 0].Icon == this.fichaO && this.casillas[1, 1].Icon == this.fichaO && this.casillas[2, 2].Icon == this.fichaO ||
-		this.casillas[0, 2].Icon == this.fichaO && this.casillas[1, 1].Icon == this.fichaO && this.casillas[2, 0].Icon == this.fichaO)
-		{
-			AnunciarDerrota();
-			this.finPartida = true;
-			return;
-		}
-	}
-
-	private void AnunciarVictoria()
-	{
-		foreach (var casilla in this.casillas)
-		{
-			casilla.Disabled = true;
-		}
-
-		Label ganador = GetNode<Label>("Ganador");
-		ganador.Text = "Victoria!";
-		ganador.AddThemeFontSizeOverride("font_size", 32);
-		ganador.HorizontalAlignment = HorizontalAlignment.Center;
-		ganador.VerticalAlignment = VerticalAlignment.Bottom;
-		ganador.Modulate = new Color(0, 255, 0);
-	}
-
-	private void AnunciarDerrota()
-	{
-		foreach (var casilla in this.casillas)
-		{
-			casilla.Disabled = true;
-		}
-
-		Label ganador = GetNode<Label>("Ganador");
-		ganador.Text = "Derrota";
-		ganador.AddThemeFontSizeOverride("font_size", 32);
-		ganador.HorizontalAlignment = HorizontalAlignment.Center;
-		ganador.VerticalAlignment = VerticalAlignment.Bottom;
-		ganador.Modulate = new Color(255, 0, 0);
+		return false;
 	}
 }
