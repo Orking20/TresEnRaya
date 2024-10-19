587078345f137d09ec796b349c5f7d9d95c1a82f
Orking20 <mateoluchetti@hotmail.com>
2024-10-19 15:03:55 +0200

    Se agrega funcionalidad para detectar al ganador

--- scripts/Tablero/Tablero.cs
+++ scripts/Tablero/Tablero.cs
@@ -8,6 +8,7 @@ public partial class Tablero : Control
 	private bool turnoJugador;
 	private Texture2D fichaX;
 	private Texture2D fichaO;
+	private bool finPartida;
 
 	public override void _Ready()
 	{
@@ -15,6 +16,7 @@ public partial class Tablero : Control
 		this.turnoJugador = true;
 		this.fichaX = (Texture2D)GD.Load("res://Sprites/Cruz.png");
 		this.fichaO = (Texture2D)GD.Load("res://Sprites/Circulo.png");
+		this.finPartida = false;
 
 		this.casillas[0, 0] = GetNode<Button>("ContenedorCasillas/Casilla1");
 		this.casillas[0, 1] = GetNode<Button>("ContenedorCasillas/Casilla2");
@@ -48,13 +50,18 @@ public partial class Tablero : Control
 			casilla.Icon = this.fichaX;
 			casilla.Disabled = true;
 			this.turnoJugador = false;
-			TurnoMaquina();
+			ChequearVictoria();
+			if (!this.finPartida)
+			{
+				TurnoMaquina();
+			}
 		}
 		else
 		{
 			casilla.Icon = this.fichaO;
 			casilla.Disabled = true;
 			this.turnoJugador = true;
+			ChequearVictoria();
 		}
 	}
 
@@ -87,5 +94,76 @@ public partial class Tablero : Control
 				columna = new Random().Next(0, 3);
 			}
 		}
+	}
+
+	private void ChequearVictoria()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (this.casillas[i, 0].Icon == this.fichaX && this.casillas[i, 1].Icon == this.fichaX && this.casillas[i, 2].Icon == this.fichaX ||
+			this.casillas[0, i].Icon == this.fichaX && this.casillas[1, i].Icon == this.fichaX && this.casillas[2, i].Icon == this.fichaX)
+			{
+				AnunciarVictoria();
+				this.finPartida = true;
+				return;
+			}
+		}
+
+		if (this.casillas[0, 0].Icon == this.fichaX && this.casillas[1, 1].Icon == this.fichaX && this.casillas[2, 2].Icon == this.fichaX ||
+		this.casillas[0, 2].Icon == this.fichaX && this.casillas[1, 1].Icon == this.fichaX && this.casillas[2, 0].Icon == this.fichaX)
+		{
+			AnunciarVictoria();
+			this.finPartida = true;
+			return;
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			if (this.casillas[i, 0].Icon == this.fichaO && this.casillas[i, 1].Icon == this.fichaO && this.casillas[i, 2].Icon == this.fichaO ||
+			this.casillas[0, i].Icon == this.fichaO && this.casillas[1, i].Icon == this.fichaO && this.casillas[2, i].Icon == this.fichaO)
+			{
+				AnunciarDerrota();
+				this.finPartida = true;
+				return;
+			}
+		}
+
+		if (this.casillas[0, 0].Icon == this.fichaO && this.casillas[1, 1].Icon == this.fichaO && this.casillas[2, 2].Icon == this.fichaO ||
+		this.casillas[0, 2].Icon == this.fichaO && this.casillas[1, 1].Icon == this.fichaO && this.casillas[2, 0].Icon == this.fichaO)
+		{
+			AnunciarDerrota();
+			this.finPartida = true;
+			return;
+		}
+	}
+
+	private void AnunciarVictoria()
+	{
+		foreach (var casilla in this.casillas)
+		{
+			casilla.Disabled = true;
+		}
+
+		Label ganador = GetNode<Label>("Ganador");
+		ganador.Text = "Victoria!";
+		ganador.AddThemeFontSizeOverride("font_size", 32);
+		ganador.HorizontalAlignment = HorizontalAlignment.Center;
+		ganador.VerticalAlignment = VerticalAlignment.Bottom;
+		ganador.Modulate = new Color(0, 255, 0);
+	}
+
+	private void AnunciarDerrota()
+	{
+		foreach (var casilla in this.casillas)
+		{
+			casilla.Disabled = true;
+		}
+
+		Label ganador = GetNode<Label>("Ganador");
+		ganador.Text = "Derrota";
+		ganador.AddThemeFontSizeOverride("font_size", 32);
+		ganador.HorizontalAlignment = HorizontalAlignment.Center;
+		ganador.VerticalAlignment = VerticalAlignment.Bottom;
+		ganador.Modulate = new Color(255, 0, 0);
 	}
 }
